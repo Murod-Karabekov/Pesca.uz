9555e89c035fb02cf27c07256d22b5ab33783ce3
Murod Karabekov <karabekovofficial@gmail.com>
2026-05-03 18:02:29 +0500

    TWA test

---
+++ twa-android/app/src/main/java/ai/smartstyle/app/DelegationService.java
@@ -0,0 +1,14 @@
+package ai.smartstyle.app;
+
+
+
+public class DelegationService extends
+        com.google.androidbrowserhelper.trusted.DelegationService {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        
+    }
+}
+
